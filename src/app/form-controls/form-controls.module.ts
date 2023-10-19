602e88558f8d9fdb3fa27ecab261e4a77ef9bee2
Simon Rabuogi <symokevo@gmail.com>
2023-10-19 17:03:29 +0300

    initia commit with from control module created

---
+++ src/app/form-controls/form-controls.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutoCompleteComponent } from './auto-complete/auto-complete.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutoCompleteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormControlsModule { }
